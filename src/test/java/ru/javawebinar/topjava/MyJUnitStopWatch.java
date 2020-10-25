6169a9d71aac338a2004096f83a39daa89388cd3
ildar <tt@tt.com>
2020-10-25 16:57:12 +0300

    add HW4 with optional

---
+++ src/test/java/ru/javawebinar/topjava/MyJUnitStopWatch.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava;
+
+import org.junit.AssumptionViolatedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MyJUnitStopWatch extends Stopwatch {
+    public static final List<String> info = new ArrayList<>();
+
+    private static void logInfo(Description description, String status, long nanos) {
+        String testName = description.getMethodName();
+        System.out.println(String.format("Test %s %s, spent %d milliseconds",
+                testName, status, TimeUnit.NANOSECONDS.toMillis(nanos)));
+        String s = String.format("Test %s %s, spent %d milliseconds",
+                testName, status, TimeUnit.NANOSECONDS.toMillis(nanos));
+        info.add(s);
+    }
+
+    @Override
+    protected void failed(long nanos, Throwable e, Description description) {
+        logInfo(description, "failed", nanos);
+    }
+
+    @Override
+    protected void skipped(long nanos, AssumptionViolatedException e, Description description) {
+        logInfo(description, "skipped", nanos);
+    }
+
+    @Override
+    protected void finished(long nanos, Description description) {
+        logInfo(description, "finished", nanos);
+    }
+}
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MyJUnitStopWatch;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -26,9 +29,11 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
-
     @Autowired
     private MealService service;
+
+    @Rule
+    public MyJUnitStopWatch stopwatch = new MyJUnitStopWatch();
 
     @Test
     public void delete() throws Exception {
@@ -107,5 +112,12 @@ public class MealServiceTest {
     @Test
     public void getBetweenWithNullDates() throws Exception {
         MEAL_MATCHER.assertMatch(service.getBetweenInclusive(null, null, USER_ID), meals);
+    }
+
+    @AfterClass
+    public static void info() {
+        MyJUnitStopWatch.info.add(0, "-------------------------------------------------------------");
+        MyJUnitStopWatch.info.add("-------------------------------------------------------------");
+        MyJUnitStopWatch.info.forEach(System.out::println);
     }
 }
